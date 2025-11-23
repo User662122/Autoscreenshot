fa7b0b72d8a40cb61a1a1b83a3aca9678735a574
User662122 <gwkrishna06@gmail.com>
2025-11-23 14:09:49 +0530

    Update build.gradle.kts

--- ScreenshotApp/app/build.gradle.kts
+++ ScreenshotApp/app/build.gradle.kts
@@ -54,5 +54,6 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
     
     
