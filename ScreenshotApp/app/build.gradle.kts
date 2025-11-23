60e155a2a7b971fa28eac04b6e21b30f527d0f56
User662122 <gwkrishna06@gmail.com>
2025-11-23 14:03:12 +0530

    Update build.gradle.kts

--- ScreenshotApp/app/build.gradle.kts
+++ ScreenshotApp/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.chessmove.detector"
+    namespace = "com.example.autoscreenshot"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.chessmove.detector"
+        applicationId = "com.example.autoscreenshot"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -49,19 +49,10 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // OpenCV
-    implementation(files("libs/opencv-4.10.0.aar"))
+    
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     
-    // OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     
-    // TensorFlow Lite
-    implementation("org.tensorflow:tensorflow-lite:2.14.0")
-    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.14.0")
-    implementation("org.tensorflow:tensorflow-lite-gpu-api:2.14.0")
-}
