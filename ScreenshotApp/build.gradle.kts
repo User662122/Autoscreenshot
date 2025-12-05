8cb15821c13211395e6a401bc469a114a8d9fb93
User662122 <gwkrishna06@gmail.com>
2025-12-05 11:51:05 +0530

    Update build.gradle.kts

--- ScreenshotApp/build.gradle.kts
+++ ScreenshotApp/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
-    // Android & Kotlin
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-
-    // Google Services plugin (Required for google-services.json)
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
-
-    // Firebase Crashlytics plugin
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 
