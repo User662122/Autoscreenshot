524736be52331a1142a7769d4596a9f0c1a5ed59
User662122 <gwkrishna06@gmail.com>
2025-12-14 11:42:46 +0530

    Update ScreenshotService.kt

--- ScreenshotApp/app/src/main/java/com/example/autoscreenshot/ScreenshotService.kt
+++ ScreenshotApp/app/src/main/java/com/example/autoscreenshot/ScreenshotService.kt
@@ -177,7 +177,7 @@ class ScreenshotService : Service() {
                     Log.d(TAG, "Bitmap resized to ${targetWidth}x$targetHeight")
                 }
 
-                val cropped = withContext(Dispatchers.Default) { cropBitmap(bitmap, 11, 436, 709, 1141) }
+                val cropped = withContext(Dispatchers.Default) { cropBitmap(bitmap, 11, 432, 712, 1131) }
 
                 // Extract 64 pieces and pass to TFLiteModelManager
                 val pieces = extract64Pieces(cropped)
