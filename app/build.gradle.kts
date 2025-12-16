3a597d940668a5d75b9bc494d6b99c65f016fdad
User662122 <gwkrishna06@gmail.com>
2025-12-16 09:07:35 +0530

    Remove ngrok integration, use Termux + Cloudflare instead
    - Removed NgrokManager.kt
    - Removed .so native libraries
    - Removed ngrok binaries from assets
    - Updated MainActivity.kt
    - Updated activity_main.xml
    - Updated build.gradle.kts (removed OkHttp)

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.screenstream"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.screenstream"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    packaging {
+        resources {
+            excludes += setOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.kotlin_module"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    
+    // WebRTC
+    implementation("io.getstream:stream-webrtc-android:1.1.3")
+    
+    // WebSocket Server
+    implementation("org.java-websocket:Java-WebSocket:1.5.3")
+    
+    // NanoHTTPD for HTTP server
+    implementation("org.nanohttpd:nanohttpd:2.3.1")
+}
